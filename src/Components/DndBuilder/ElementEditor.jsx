df48d6df0f1e2f114ce067b4220e2606b00ea8e8
Guruvignesh M <vigneshguru274@gmail.com>
2023-04-26 23:42:46 +0530

    add the close sections

--- src/Components/DndBuilder/ElementEditor.jsx
+++ src/Components/DndBuilder/ElementEditor.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { DndState } from "../../context/DndProvider";
+import { QuestionTextEditor } from "../Editor/questionTextEditor";
 
 export function ElementEditor() {
+  const { editableElement } = DndState();
   return (
-    <div className="bg-red-300">ElementEditor</div>
-  )
+    <div className="bg-red-300">
+      <div className="bg-blue-400 cursor-pointer justify-center flex items-center">
+        close
+      </div>
+      {ElementChecker(editableElement)}
+    </div>
+  );
+}
+function ElementChecker(ele) {
+  if (ele.id.includes("question")) return <QuestionTextEditor />;
+  return <div>Unwanted dropdown</div>;
 }
