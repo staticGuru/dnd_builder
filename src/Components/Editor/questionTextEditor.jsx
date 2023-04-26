08c734e91f31fcd33cb7486d6e31715d62e0520c
Guruvignesh M <vigneshguru274@gmail.com>
2023-04-27 00:15:35 +0530

    Add the save changes sections

--- src/Components/Editor/questionTextEditor.jsx
+++ src/Components/Editor/questionTextEditor.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { DndState } from "../../context/DndProvider";
 
 export function QuestionTextEditor() {
-  const { editableElement } = DndState();
-  return <div>QuestionTextEditor</div>;
+  const { editableElement,updateElementProperty } = DndState();
+  const [value,setValue]=useState("");
+  const item={...editableElement}
+  function handleValueChange(event){
+     setValue(event.target.value);
+  }
+  function saveChanges(){
+     item.content=value;
+     updateElementProperty(editableElement,item);
+  }
+  return (
+    <div>
+      <div>
+        <span>Question</span>
+        <input placeholder="Enter question here" value={value} onChange={handleValueChange}/>
+
+      </div>
+      <div className="cursor-pointer" onClick={saveChanges}>Save</div>
+    </div>
+  );
 }
