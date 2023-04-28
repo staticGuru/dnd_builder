e2d626e5e9c4b31c80593764976d73250bef2df5
Guruvignesh M <vigneshguru274@gmail.com>
2023-04-28 20:16:35 +0530

    add slides list sections

--- src/Components/DndBuilder/SlideLists.jsx
+++ src/Components/DndBuilder/SlideLists.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { DndState } from "../../context/DndProvider";
 
 export function SlideLists() {
+  const { exportedQuestionaire, activeSlide, setActiveSlide } = DndState();
   return (
     <div className="bg-slate-600 flex flex-col">
-      <div>Guru</div>
-      <div>Guru</div>
-      <div>Guru</div>
+      {exportedQuestionaire.map((slide, index) => (
+        <div
+          key={index}
+          className={`${
+            index === activeSlide ? "bg-green-500" : "bg-slate-500"
+          }`}
+        >
+          <span
+            className="cursor-pointer"
+            onClick={() => setActiveSlide(index)}
+          >
+            {"slide-" + (index + 1)}
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
