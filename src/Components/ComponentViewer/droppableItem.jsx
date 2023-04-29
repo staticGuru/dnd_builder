9e45a7090e7766ecc0b33319e8302c9e189b32dc
Guruvignesh M <vigneshguru274@gmail.com>
2023-04-29 16:53:26 +0530

    add the droppable items

--- src/Components/ComponentViewer/droppableItem.jsx
+++ src/Components/ComponentViewer/droppableItem.jsx
@@ -36,11 +36,12 @@ export function DroppableItems({ provided, snapshot, item }) {
         {...provided.dragHandleProps}
         style={{
           userSelect: "none",
-        display:"flex",
-        flexDirection:"row",
+          display: "flex",
+          flexDirection: "row",
           minHeight: "50px",
           width: "80%",
           alignSelf: "center",
+          marginTop:"10px",
           overflow: "hidden",
           backgroundColor: snapshot.isDragging ? "#263B4A" : "#F4EEE0",
           color: "black",
@@ -49,11 +50,16 @@ export function DroppableItems({ provided, snapshot, item }) {
         className="rounded-xl border-2 border-black font-semibold text-base"
         onClick={() => handleElementEditor(item)}
       >
-        <div style={{backgroundColor:"#3795BD",padding:16}} className="font-bold text-base">B</div>
-        <div style={{padding:16,paddingLeft:8}}>{item.content}</div>
+        <div
+          style={{ backgroundColor: "#3795BD", padding: 16 }}
+          className="font-bold text-base"
+        >
+          {item.optionKey}
+        </div>
+        <div style={{ padding: 16, paddingLeft: 8 }}>{item.content}</div>
       </div>
     );
-    if (item.id.includes("image"))
+  if (item.id.includes("image"))
     return (
       <div
         ref={provided.innerRef}
@@ -61,20 +67,26 @@ export function DroppableItems({ provided, snapshot, item }) {
         {...provided.dragHandleProps}
         style={{
           userSelect: "none",
-        display:"flex",
-        flexDirection:"row",
+          display: "flex",
+          flexDirection: "row",
           minHeight: "50px",
           width: "80%",
           alignSelf: "center",
           overflow: "hidden",
-          backgroundColor: snapshot.isDragging ? "#263B4A" : "#F4EEE0",
-          color: "black",
+          backgroundColor: snapshot.isDragging ? "#DAF5FF" : "transparent",
           ...provided.draggableProps.style,
         }}
-        className="rounded-xl border-2 border-black font-semibold text-base"
         onClick={() => handleElementEditor(item)}
       >
-        <img alt="#" src={item.imageUrl} style={{width:`${item.width}px`,height:`${item.height}px`}}/>
+        <img
+          alt="#"
+          src={item.imageUrl}
+          style={{ width: `${item.width ?? 80}px`, height: `${item.height ??80}px` }}
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null; // prevents looping
+            currentTarget.src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
+          }}
+        />
       </div>
     );
   return (
