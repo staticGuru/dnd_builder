16024993ef3235e2664009691c1e20994c970cf8
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-06 11:29:40 +0530

    add the summery view editors

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { DndState } from '../context/DndProvider';
+import { PreviewTemplate } from './DndPreviewer/PreviewTemplate';
 
 const Summary = () => {
+    const {templateData,exportedQuestionaire}=DndState()
+  
     return (
         <div className='container mt-5 shadow back p-5'>
             <div className='row'>
@@ -19,19 +23,19 @@ const Summary = () => {
                 <div className='col-lg-3'>
                     <div className='mt-4'>
                         <label>Template Name</label>
-                        <input type="text" placeholder="Survey" className="form-control form" />
+                        <input disabled value={templateData?.name ?? ""} type="text" placeholder="Survey" className="form-control form" />
                     </div>
                 </div>
                 <div className='col-lg-3'>
                     <div className='mt-4 offset-2'>
                         <label>Template Type</label>
-                        <input type="text" placeholder="Questionanaire" className="form-control form" />
+                        <input disabled value={templateData?.type ?? ""} type="text" placeholder="Questionanaire" className="form-control form" />
                     </div>
                 </div>
                 <div className='col-lg-3'>
                     <div className='mt-4 offset-2'>
                         <label>Category</label>
-                        <input type="text" placeholder="Q & A" className="form-control form" />
+                        <input disabled value={templateData?.que ?? ""} type="text" placeholder="Q & A" className="form-control form" />
                     </div>
                 </div>
             </div>
@@ -40,14 +44,14 @@ const Summary = () => {
                 <div className='col-lg-7'>
                     <div className='mt-4'>
                         <label> Description </label>
-                        <textarea className='form-control form'></textarea>
+                        <textarea disabled value={templateData?.description ?? ""} className='form-control form'></textarea>
                     </div>
                 </div>
             </div>
             <div className='row'>
-                <div className='col-lg-7'>
-                    <div className='mt-4'>
-                        <textarea className='form-control form'></textarea>
+                <div className='col-lg-12'>
+                    <div className='mt-4 bg-gray-200 h-44 border-2 border-gray-300 rounded-md overflow-hidden px-3 py-2 flex '>
+                        {exportedQuestionaire.map((item,index)=>(<PreviewTemplate item={item} index={index}/>))}
                     </div>
                 </div>
             </div>
