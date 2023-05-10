524d68b962c613794c5f7183d2a152f25a9c4b59
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-10 18:08:14 +0530

    add the toolbox api

--- src/Components/craft/components/Toolbox.js
+++ src/Components/craft/components/Toolbox.js
@@ -1,76 +1,138 @@
-import { useEditor, Element } from '@craftjs/core';
+import { useEditor, Element } from "@craftjs/core";
 import {
   Box,
   Typography,
   Grid,
   Button as MaterialButton,
-} from '@material-ui/core';
-import React from 'react';
-
-import { Button } from './user/Button';
-import { Card } from './user/Card';
-import { Container } from './user/Container';
-import { Text } from './user/Text';
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@material-ui/core";
+import React from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import QuizOutlinedIcon from "@mui/icons-material/QuizOutlined";
+import QuestionAnswerOutlinedIcon from "@mui/icons-material/QuestionAnswerOutlined";
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import { Button } from "./user/Button";
+import { Card } from "./user/Card";
+import { Container } from "./user/Container";
+import { Text } from "./user/Text";
+import ImageContent from "./user/ImageContent";
 
 export const Toolbox = () => {
   const { connectors } = useEditor();
 
   return (
-    <Box px={2} py={2}>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justify="center"
-        spacing={1}
-      >
-        <Box pb={2}>
-          <Typography>Drag to add</Typography>
-        </Box>
-        <Grid container direction="column" item>
-          <MaterialButton
-            ref={(ref) =>
-              connectors.create(ref, <Button text="Click me" size="small" />)
-            }
-            variant="contained"
-            data-cy="toolbox-button"
-          >
-            Button
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
-            ref={(ref) => connectors.create(ref, <Text text="Hi world" />)}
-            variant="contained"
-            data-cy="toolbox-text"
-          >
-            Text
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
-            ref={(ref) =>
-              connectors.create(
-                ref,
-                <Element canvas is={Container} padding={20} />
-              )
-            }
-            variant="contained"
-            data-cy="toolbox-container"
-          >
-            Container
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
-            ref={(ref) => connectors.create(ref, <Card />)}
-            variant="contained"
-            data-cy="toolbox-card"
-          >
-            Card
-          </MaterialButton>
-        </Grid>
-      </Grid>
+    <Box>
+      <Accordion>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography>Add Components</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Box
+                ref={(ref) =>
+                  connectors.create(ref, <Text text="Add your questions.." />)
+                }
+                variant="contained"
+                data-cy="toolbox-question"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                }}
+              >
+                <QuizOutlinedIcon />
+                <span>Question</span>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box
+                ref={(ref) =>
+                  connectors.create(ref, <Text text="Enter Options" />)
+                }
+                variant="contained"
+                data-cy="toolbox-text"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer"
+                }}
+              >
+                <QuestionAnswerOutlinedIcon />
+                <span>Answers</span>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box
+                ref={(ref) =>
+                  connectors.create(ref, <ImageContent />)
+                }
+                variant="contained"
+                data-cy="toolbox-text"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer"
+                }}
+              >
+                <ImageOutlinedIcon />
+                <span>Image</span>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <MaterialButton
+                ref={(ref) =>
+                  connectors.create(
+                    ref,
+                    <Element canvas is={Container} padding={20} />
+                  )
+                }
+                variant="contained"
+                data-cy="toolbox-container"
+              >
+                Container
+              </MaterialButton>
+            </Grid>
+            <Grid item xs={6}>
+              <MaterialButton
+                ref={(ref) => connectors.create(ref, <Card />)}
+                variant="contained"
+                data-cy="toolbox-card"
+              >
+                Card
+              </MaterialButton>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography>Add Action Items</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <MaterialButton
+                ref={(ref) =>
+                  connectors.create(ref, <Button text="Submit" size="small" />)
+                }
+                variant="contained"
+                data-cy="toolbox-submit-button"
+              >
+                Submit
+              </MaterialButton>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   );
 };
