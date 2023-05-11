d63a71de209927675db1f26ee448a6e8654a557f
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-11 23:51:36 +0530

    add the slide sections sextions

--- src/Components/DndBuilder/SlideLists.jsx
+++ src/Components/DndBuilder/SlideLists.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { DndState } from "../../context/DndProvider";
+import { useEditor } from "@craftjs/core";
 
 export function SlideLists() {
   const { exportedQuestionaire, activeSlide, setActiveSlide } = DndState();
+  const {actions}=useEditor();
+  function changeActiveSlide(index){
+    setActiveSlide(index);
+    let JsonObj=exportedQuestionaire[index].json
+    console.log({JsonObj})
+    actions.deserialize(JsonObj)
+  }
   return (
-    <div className="bg-slate-600 flex flex-col p-3">
+    <div className="bg-slate-100 flex flex-col p-3">
       {exportedQuestionaire.map((slide, index) => (
         <div
           key={index}
           className={`h-32 w-28 bg-white text-black font-semibold rounded-xl border-4 ${
             index === activeSlide ? "border-yellow-400" : "border-black"
           } text-center items-end cursor-pointer mb-2`}
-          onClick={() => setActiveSlide(index)}
+          onClick={() => changeActiveSlide(index)}
         >
           <span className="cursor-pointer">{"slide-" + (index + 1)}</span>
         </div>
