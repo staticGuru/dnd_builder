295b0cfad3153aa7f1792a3eee2ac6156ff7bd97
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-19 16:14:23 +0530

    add the one column component

---
+++ src/Components/craft/components/Layout/oneColumn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '../user/Container'
+import { Element } from '@craftjs/core'
+
+export function OneColumn() {
+  return (
+     <Element id="data-1" canvas is={Container} padding={5}/>
+  )
+}
+
