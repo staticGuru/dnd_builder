1077045a77288097b7940cf2cf9a987d9e7d518d
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-20 23:12:04 +0530

    fix the double router issues

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "grapesjs/dist/css/grapes.min.css";
-import Editor from './Components/Editor';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './Components/style.css';
+import Editor from "./Components/Editor";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./Components/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
