12dd0b33cbe038574b07dee8c47a3fd9a321d0f4
Guruvignesh M <vigneshguru274@gmail.com>
2023-05-20 12:20:52 +0530

    add the layout sections

---
+++ src/Components/craft/components/Layout/index.js
@@ -0,0 +1,81 @@
+import { Element, useEditor } from '@craftjs/core';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, Typography } from '@material-ui/core'
+import React from 'react'
+import {Container} from "../user/Container";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ViewColumnOutlinedIcon from '@mui/icons-material/ViewColumnOutlined';
+import { OneColumn } from './oneColumn';
+import { TwoColumn } from './twoColumn';
+import { ThreeColumn } from './threeColumn';
+export function LayoutComponent() {
+     const { connectors } = useEditor();
+
+  return (
+     <Accordion>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography>Layout</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+            <Box
+            ref={(ref) =>
+              connectors.create(ref,  <OneColumn/>)
+            }
+            variant="contained"
+            data-cy="toolbox-question"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <ViewColumnOutlinedIcon />
+            <span>One Column</span>
+          </Box>
+            </Grid>
+            <Grid item xs={6}>
+            <Box
+            ref={(ref) =>
+              connectors.create(ref, <TwoColumn />)
+            }
+            variant="contained"
+            data-cy="toolbox-question"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <ViewColumnOutlinedIcon />
+            <span>Two Column</span>
+          </Box>
+            </Grid>
+            <Grid item xs={6}>
+            <Box
+            ref={(ref) =>
+              connectors.create(ref, <ThreeColumn/>)
+            }
+            variant="contained"
+            data-cy="toolbox-question"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <ViewColumnOutlinedIcon />
+            <span>Three Column</span>
+          </Box>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+  )
+}
